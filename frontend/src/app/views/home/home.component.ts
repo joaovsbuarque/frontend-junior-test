99052c9f39a3c00fe4912ab70c47eb6548ef2707
joaovsbuarque <joaobuarqueestudos@gmail.com>
2023-01-13 20:56:48 -0300

    fix: change home page

--- frontend/src/app/views/home/home.component.ts
+++ frontend/src/app/views/home/home.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
+import { User } from "src/app/components/template/user.model";
+import { gitUser } from "src/app/components/template/gitUser.model";
+import { UserService } from "src/app/service/user.service";
+import { RepositoriosService } from "src/app/service/repositorios.service";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.css"],
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  username: string | undefined;
 
+  constructor(
+    private router: Router,
+    public userService: UserService,
+    public repositoriesService: RepositoriosService
+  ) {}
+  ngOnInit(): void {}
+
+  getUsers(resp: string) {
+    this.userService.getUsers(resp).subscribe((data) => {
+      this.repositoriesService.getRepositorios(resp).subscribe((repo) => {
+        this.navigateToUserPage({ userData: data, repoData: repo });
+      });
+    });
+  }
+
+  navigateToUserPage(data: gitUser): void {
+    this.router.navigate(["/user"], { state: { data: data } });
+  }
 }
