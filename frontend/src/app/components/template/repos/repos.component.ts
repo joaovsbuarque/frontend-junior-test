8975beabf9662858fd8ed994388e8a5ee42e83a5
joaovsbuarque <joaobuarqueestudos@gmail.com>
2023-01-13 20:52:00 -0300

    feat: inprovements repo component

--- frontend/src/app/components/template/repos/repos.component.ts
+++ frontend/src/app/components/template/repos/repos.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Repositorios } from "../repos.model";
 
 @Component({
-  selector: 'app-repos',
-  templateUrl: './repos.component.html',
-  styleUrls: ['./repos.component.css']
+  selector: "app-repos",
+  templateUrl: "./repos.component.html",
+  styleUrls: ["./repos.component.css"],
 })
-export class ReposComponent {
-
+export class ReposComponent implements OnInit {
+  userRepos: Array<Repositorios> = [];
+  constructor() {}
+  ngOnInit(): void {
+    this.userRepos = history.state.data.repoData.sort(
+      (a: Repositorios, b: Repositorios) => {
+        return a.stargazers_count - b.stargazers_count;
+      }
+    );
+  }
 }
